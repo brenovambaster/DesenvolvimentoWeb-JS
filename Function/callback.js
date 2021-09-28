6e732901a3a01a23b0370dc20319c8f1e4432c79
Breno Vambáster <brenovambaster5@gmail.com>
2021-09-28 14:29:22 -0300

    update

--- Function/callback.js
+++ Function/callback.js
@@ -2,7 +2,7 @@ const fabricantes = ['mercedes', 'ford', 'toyota'];
 
 imprimir = (nome, indice) => console.log(`${indice + 1}.${nome}`)
 
-// aqui o callback
+// aqui a callback
 fabricantes.forEach(a => {
     return console.log(`${a}`);
 });
