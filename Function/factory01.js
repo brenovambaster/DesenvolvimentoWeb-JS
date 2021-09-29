a05e9eb44f92f42c61aa08db7730235f382435b6
Breno Vambáster <brenovambaster5@gmail.com>
2021-09-28 21:51:47 -0300

    update factory function

--- Function/factory01.js
+++ Function/factory01.js
@@ -1,25 +1,25 @@
 const prod1 = {
-        name: 'sabao',
-        price: 12,
-        qtd: 13,
-        _valor() {
-            console.log(this.price)
-        },
-        _name: () => {
-            return console.log(prod1.name) // arrow function tem this != 
-        },
-        _name2: function() {
-            return console.log(this.name) // this = prod1
-        },
-        _name3() {
-            return console.log(this.name)
-        }
+    name: 'sabao',
+    price: 12,
+    qtd: 13,
+    _valor() {
+        console.log(this.price)
+    },
+    _name: () => { // atributo "_name:" recebe uma função
+        return console.log("prod1 " + prod1.name) // arrow function tem this != 
+    },
+    _name2: function() {
+        return console.log(this.name) // this = prod1
+    },
+    _name3() {
+        return console.log(this.name)
     }
-    //console.log(prod1._name3())
+}
+console.log(prod1._name2())
 
 // function factory
-function criarProd(name2 = 'indefinido', price2 = 0, amount2 = 0) {
-    return prod = {
+function criarProd(name2 = 'indefinido', price2 = 1, amount2 = 1) {
+    return prod2 = {
         /* ou assim  
             name, 
             price, 
@@ -30,7 +30,7 @@ function criarProd(name2 = 'indefinido', price2 = 0, amount2 = 0) {
         amount: amount2,
         _name: function() { console.log(this.name) },
         _pirce() { console.log(this.price) },
-        _qtd: () => { console.log(prod.amount) },
+        _qtd: () => { console.log(prod2.amount) },
         seeData() {
             console.log('name: ' + this.name, 'price: ' + this.price, 'qtd: ' + this.amount)
         }
@@ -38,8 +38,8 @@ function criarProd(name2 = 'indefinido', price2 = 0, amount2 = 0) {
 
 }
 
-//const pord2 = criarProd()
-//pord2.seeData()
+const pord2 = criarProd() // caso não passe argumentos, a função inicia os argumentos por padrão pré-definidos.
+pord2.seeData()
 
 // factory
 const pessoa = (nome) => {
