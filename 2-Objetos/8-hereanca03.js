c2e2119bc09850a1bddd7a488ccc418ffe7ceaf4
Breno Vambáster <brenovambaster5@gmail.com>
2021-10-04 10:24:38 -0300

    completed week 1. #Object and Class

--- 2-Objetos/8-hereanca03.js
+++ 2-Objetos/8-hereanca03.js
@@ -4,3 +4,9 @@ const filha1 = Object.create(pai);
 filha1.nome = 'ana'
 console.log(filha1.nome)
 console.log(filha1.cabelo)
+
+const filha2 = Object.create(pai, {
+    nome: { value: 'bia', writable: true, enumerable: true },
+    idade: { value: 17, writable: true, enumerable: true }
+})
+console.log(filha2, filha2.cabelo)
