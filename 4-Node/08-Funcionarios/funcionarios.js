f3a504161a80cffa8fb89bbf0afc2f34c73dd6e3
Breno Vambáster <brenovambaster5@gmail.com>
2022-05-25 18:17:32 -0300

    08-funcionarios

---
+++ 4-Node/08-Funcionarios/funcionarios.js
@@ -0,0 +1,16 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const china = (e) => e.pais == "China"
+const mulheres = (f) => f.genero == "F"
+
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionario = response.data
+    const funcionaria = funcionario.filter(china).filter(mulheres).reduce(menorSalario)
+    console.log(funcionaria)
+
+})
