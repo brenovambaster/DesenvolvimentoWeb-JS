b7dbb0753f67f0ab499e12132129b4d5d561b22d
breno <brenovambaster5@gmail.com>
2022-08-02 19:01:48 -0300

    update

--- 4-Node/4.2-Funcionarios/funcionarios.js
+++ 4-Node/4.2-Funcionarios/funcionarios.js
@@ -1,3 +1,5 @@
+/* DESAFIO AULA 154 */
+
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
---
+++ 4-Node/4.2-Funcionarios/resolucao_desafio.js
@@ -0,0 +1,35 @@
+/* Extrair a MULHER CHINESA com o   MENOR SALÁRIOS */
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+const axios = require('axios')
+axios.get(url).then(response => {
+    const funcionarios = response.data;
+    const funcionarios2 = funcionarios;
+
+
+    //  CODE SMEL
+    /* Dentro do filter E reduce  passa-se uma callback */
+    const funcionaria = funcionarios.filter((element, index) => {
+        return element.pais === 'China' && element.genero == 'F'
+    })
+
+    let result = funcionaria.reduce((accumulador, current) => {
+        return (accumulador.salario < current.salario) ? accumulador : current;
+    })
+    console.log(result)
+
+    /* -------- REFACTORING CODE ---------- */
+    const pais_China = f => f.pais === 'China';
+    const mulher = e => e.genero === 'F';
+    const menor_salario = (acc, cur) => {
+        return (acc.salario < cur.salario) ? acc : cur;
+    }
+
+    let func = funcionarios2
+        .filter(pais_China)
+        .filter(mulher)
+        .reduce(menor_salario);
+
+    console.log(func)
+
+})
