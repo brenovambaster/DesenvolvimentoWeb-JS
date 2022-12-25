c4d8164453ae885e7527ba2a2307edcfc79a57ab
breno <brenovambaster5@gmail.com>
2022-12-25 19:54:59 -0300

    api: create,read, update, delete

--- 4-Node/ProjetoAPI/src/server.js
+++ 4-Node/ProjetoAPI/src/server.js
@@ -1,13 +1,42 @@
 const port = 3003;
 const express = require('express');
+const bd = require('./bd');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: "Sabao em po", qtd: 12, valor: 12.43, })
+    res.send(bd.getTodosProdutos())
     //console.log(req);
 })
 
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bd.getProduto(req.params.id))
+})
+
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const { nome, preco } = req.body;
+    const produto = bd.salvarProduto({
+        id: req.params.id,
+        nome, preco
+    })
+    res.send(produto);
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    bd.excluirProduto(req.params.id);
+    res.send(bd.getTodosProdutos())
+})
 app.listen(port, () => {
     console.log(`Server start, port: ${port}`);
 })
